7d17c5859c9db74cb2be1441abed43af71ac0632
Irfan Sharif <irfanmahmoudsharif@gmail.com>
2016-02-20 06:01:47 -0500

    initial commit, clean ubuntu setup

---
+++ install/bootstrap.sh
@@ -0,0 +1,11 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install -f
+
+sudo apt-get install google-chrome-stable git-core vim zsh
+
+sudo curl -L http://install.ohmyz.sh > ~/install.sh
+sudo sh ~/install.sh
+sudo wget -nocheck-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O – | sh
+sudo chsh -s /bin/zsh
+rm ~/install.sh
