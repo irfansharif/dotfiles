ec1e560f2468794a4a2a254fc3e0ee589adaec96
Irfan Sharif <irfanmahmoudsharif@gmail.com>
2016-03-01 10:28:47 -0500

    vim plugin management/config

--- install/terminal.sh
+++ install/terminal.sh
@@ -12,3 +12,7 @@ rm ~/install.sh
 
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
+git clone git://github.com/airblade/vim-gitgutter.git ~/.vim/bundle/vim-gitgutter
