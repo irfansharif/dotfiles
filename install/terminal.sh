b2dad23b013ea92c0aec914b0dff41469c2b7517
Irfan Sharif <irfanmahmoudsharif@gmail.com>
2016-03-02 11:03:14 -0500

    cleaned up vimrc config

--- install/terminal.sh
+++ install/terminal.sh
@@ -16,3 +16,4 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
 git clone git://github.com/airblade/vim-gitgutter.git ~/.vim/bundle/vim-gitgutter
+git clone http://github.com/sjl/gundo.vim.git ~/.vim/bundle/gundo
